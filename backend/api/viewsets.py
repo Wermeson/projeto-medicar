1bf228a8f9eab54dd871f846c9583c23676a7490
Wermeson <franciscowermeson@gmail.com>
2021-09-16 15:16:18 -0300

    COrreção na listagem de consulsultas

--- backend/api/viewsets.py
+++ backend/api/viewsets.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import filters as filters_rest
 from rest_framework.generics import ListAPIView, CreateAPIView
 from backend.filters import *
+from datetime import date, datetime
+from django.db.models import Q
 
 class EspecialidadeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.EspecialidadeSerializer
@@ -29,10 +31,15 @@ class AgendaViewSet(viewsets.ModelViewSet):
 
 class ConsultaViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ConsultaSerializer
+    permission_classes = [IsAuthenticated]
     # queryset = Consulta.objects.all()
-
     def get_queryset(self):
-        return Consulta.objects.filter(usuario=self.request.user)
+        consultas = Consulta.objects.filter(usuario=self.request.user, agenda__dia__gte=date.today())
+        # for consulta in consultas:
+        #     if consulta.agenda.dia == date.today() and consulta.horario.horario < datetime.now().time(0):
+        return consultas.exclude(agenda__dia=date.today(), horario__horario__lt=datetime.now().time())
+
+        # return Consulta.objects.filter(Q(usuario=self.request.user) & Q(agenda__dia__gte=date.today()) | (Q(agenda__dia=date.today()) & Q(horario__horario__gte=datetime.now().time())))
 
 
 class UserViewSet(CreateAPIView):
