0b17b69049802ab5b305da650ebde14302d94a0e
Wermeson Reis <franciscowermeson@gmail.com>
2022-05-17 08:37:29 -0300

    Inserido responsividade no componente Nav

--- frontend/src/app/components/template/nav/nav.component.ts
+++ frontend/src/app/components/template/nav/nav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver){
+
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 800px)']).subscribe((res)=> {
+      if(res.matches){
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
   }
 
 }
